c67c4b228dcc0f7531cd8227e116c4352354e96f
zlatovladdka <vlad.temkin@gmail.com>
2021-12-27 00:16:39 +0300

    add seir + pres changes

--- seir/seir_exposure.jl
+++ seir/seir_exposure.jl
@@ -1,10 +1,16 @@
+cd(@__DIR__)
+using Pkg; Pkg.activate("."); Pkg.instantiate()
+
+# Single experiment, move to ensemble further on
+# Some good parameter values are stored as comments right now
+# because this is really good practice
+
 using OrdinaryDiffEq
 using ModelingToolkit
 using DataDrivenDiffEq
-using DiffEqSensitivity, Optim
+using LinearAlgebra, DiffEqSensitivity, Optim
 using DiffEqFlux, Flux
 using Plots
-using DifferentialEquations
 gr()
 
 function corona!(du,u,p,t)
@@ -21,33 +27,27 @@ function corona!(du,u,p,t)
     du[1] = dS; du[2] = dE; du[3] = dI; du[4] = dR
     du[5] = dN; du[6] = dD; du[7] = dC
 end
-
 β(t,β0,D,N,κ,α) = β0*(1-α)*(1-D/N)^κ
 S0 = 14e6
 u0 = [0.9*S0, 0.0, 0.0, 0.0, S0, 0.0, 0.0]
 p_ = [10.0, 0.5944, 0.4239, 1117.3, 0.02, 1/3, 1/5,0.2, 1/11.2]
 R0 = p_[2]/p_[7]*p_[6]/(p_[6]+p_[5])
 tspan = (0.0, 21.0)
-
 prob = ODEProblem(corona!, u0, tspan, p_)
 solution = solve(prob, Vern7(), abstol=1e-12, reltol=1e-12, saveat = 1)
-scatter(solution)
 
 tspan2 = (0.0,60.0)
 prob = ODEProblem(corona!, u0, tspan2, p_)
 solution_extrapolate = solve(prob, Vern7(), abstol=1e-12, reltol=1e-12, saveat = 1)
-scatter!(solution_extrapolate)
 
 # Ideal data
 tsdata = Array(solution)
 # Add noise to the data
 noisy_data = tsdata + Float32(1e-5)*randn(eltype(tsdata), size(tsdata))
 
-tsdata_extrapolate = Array(solution_extrapolate)
-noisy_data_extrapolate = tsdata_extrapolate + Float32(1e-5)*randn(eltype(tsdata_extrapolate), size(tsdata_extrapolate))
-
 plot(abs.(tsdata-noisy_data)')
-scatter(noisy_data')
+
+### Neural ODE
 
 ann_node = FastChain(FastDense(7, 64, tanh),FastDense(64, 64, tanh), FastDense(64, 64, tanh), FastDense(64, 7))
 p = Float64.(initial_params(ann_node))
@@ -62,12 +62,8 @@ function dudt_node(u,p,t)
 
     [dS,dE,dI,dR,dN,dD,dC]
 end
-
 prob_node = ODEProblem(dudt_node, u0, tspan, p)
 s = concrete_solve(prob_node, Tsit5(), u0, p, saveat = solution.t)
-
-plot(s)
-scatter!(solution)
 
 function predict(θ)
     Array(concrete_solve(prob_node, Vern7(), u0, θ, saveat = 1,
@@ -75,6 +71,7 @@ function predict(θ)
                          sensealg = InterpolatingAdjoint(autojacvec=ReverseDiffVJP())))
 end
 
+# No regularisation right now
 function loss(θ)
     pred = predict(θ)
     sum(abs2, (noisy_data[2:4,:] .- pred[2:4,:])), pred # + 1e-5*sum(sum.(abs, params(ann)))
@@ -91,10 +88,8 @@ callback(θ,l,pred) = begin
     false
 end
 
-res1_node = DiffEqFlux.sciml_train(loss, p, ADAM(0.1), cb=callback, maxiters = 500)
+res1_node = DiffEqFlux.sciml_train(loss, p, ADAM(0.01), cb=callback, maxiters = 500)
 res2_node = DiffEqFlux.sciml_train(loss, res1_node.minimizer, BFGS(initial_stepnorm=0.01), cb=callback, maxiters = 10000)
-
-losses
 
 prob_node2 = ODEProblem(dudt_node, u0, tspan, res2_node.minimizer)
 s = solve(prob_node2, Tsit5(), saveat = 1)
@@ -104,6 +99,7 @@ plot!(s, vars=[2,3,4], label=["Estimated Exposed" "Estimated Infected" "Estimate
 # Plot the losses
 plot(losses, yaxis = :log, xaxis = :log, xlabel = "Iterations", ylabel = "Loss")
 
+# Extrapolate out
 prob_node_extrapolate = ODEProblem(dudt_node,u0, tspan2, res2_node.minimizer)
 _sol_node = solve(prob_node_extrapolate, Vern7(), abstol=1e-12, reltol=1e-12, saveat = 1)
 p_node = scatter(solution_extrapolate, vars=[2,3,4], legend = :topleft, label=["True Exposed" "True Infected" "True Recovered"], title="Neural ODE Extrapolation")
@@ -112,7 +108,6 @@ plot!(p_node,[20.99,21.01],[0.0,maximum(hcat(Array(solution_extrapolate[2:4,:]),
 
 savefig("neuralode_extrapolation.png")
 savefig("neuralode_extrapolation.pdf")
-
 
 ### Universal ODE Part 1
 
@@ -133,12 +128,11 @@ function dudt_(u,p,t)
 
     [dS,dE,dI,dR,dN,dD,dC]
 end
-
 prob_nn = ODEProblem(dudt_,u0, tspan, p)
 s = concrete_solve(prob_nn, Tsit5(), u0, p, saveat = 1)
 
 plot(solution, vars=[2,3,4])
-scatter!(s[2:4,:]')
+plot!(s[2:4,:]')
 
 function predict(θ)
     Array(concrete_solve(prob_nn, Vern7(), u0, θ, saveat = solution.t,
@@ -146,7 +140,7 @@ function predict(θ)
                          sensealg = InterpolatingAdjoint(autojacvec=ReverseDiffVJP())))
 end
 
-
+# No regularisation right now
 function loss(θ)
     pred = predict(θ)
     sum(abs2, noisy_data[2:4,:] .- pred[2:4,:]), pred # + 1e-5*sum(sum.(abs, params(ann)))
@@ -170,12 +164,10 @@ loss(res2_uode.minimizer)
 
 prob_nn2 = ODEProblem(dudt_,u0, tspan, res2_uode.minimizer)
 uode_sol = solve(prob_nn2, Tsit5(), saveat = 1)
-scatter(solution, vars=[2,3,4], label="Real solution")
-plot!(uode_sol, vars=[2,3,4], label="UODE prediction")
+plot(solution, vars=[2,3,4])
+plot!(uode_sol, vars=[2,3,4])
 
-savefig("uode_prediction_train.png")
-savefig("uode_prediction_train.pdf")
-
+# Plot the losses
 plot(losses, yaxis = :log, xaxis = :log, xlabel = "Iterations", ylabel = "Loss")
 
 # Collect the state trajectory and the derivatives
@@ -183,6 +175,7 @@ X = noisy_data
 # Ideal derivatives
 DX = Array(solution(solution.t, Val{1}))
 
+# Extrapolate out
 prob_nn2 = ODEProblem(dudt_,u0, tspan2, res2_uode.minimizer)
 _sol_uode = solve(prob_nn2, Vern7(), abstol=1e-12, reltol=1e-12, saveat = 1)
 p_uode = scatter(solution_extrapolate, vars=[2,3,4], legend = :topleft, label=["True Exposed" "True Infected" "True Recovered"], title="Universal ODE Extrapolation")
@@ -197,12 +190,10 @@ savefig("universalode_extrapolation.pdf")
 # Create a Basis
 @variables u[1:3]
 # Lots of polynomials
-polys = []
+polys = Operation[]
 for i ∈ 0:2, j ∈ 0:2, k ∈ 0:2
     push!(polys, u[1]^i * u[2]^j * u[3]^k)
 end
-
-polys
 
 # And some other stuff
 h = [cos.(u)...; sin.(u)...; unique(polys)...]
@@ -211,103 +202,49 @@ basis = Basis(h, u)
 X = noisy_data
 # Ideal derivatives
 DX = Array(solution(solution.t, Val{1}))
-S,E,i,R,N,D,C = eachrow(X)
+S,E,I,R,N,D,C = eachrow(X)
 F,β0,α,κ,μ,_,γ,d,λ = p_
-L = β.(0:tspan[end],β0,D,N,κ,α).*S.*i./N
-L̂ = vec(ann([S./N i D./N]',res2_uode.minimizer))
-X̂ = [S./N i D./N]'
-
-
-X_ext = noisy_data_extrapolate
-DX_ext = Array(solution(solution_extrapolate.t, Val{1}))
-S,E,i,R,N,D,C = eachrow(X_ext)
-F,β0,α,κ,μ,_,γ,d,λ = p_
-L_ext = β.(0:tspan2[end],β0,D,N,κ,α).*S.*i./N
-
+L = β.(0:tspan[end],β0,D,N,κ,α).*S.*I./N
+L̂ = vec(ann([S./N I D./N]',res2_uode.minimizer))
+X̂ = [S./N I D./N]'
 
 scatter(L,title="Estimated vs Expected Exposure Term",label="True Exposure")
 plot!(L̂,label="Estimated Exposure")
-savefig("uode_estimated_exposure.png")
-savefig("uode_estimated_exposure.pdf")
-
-
-
-
-# Create the thresholds which should be used in the search process
-thresholds = Float32.(exp10.(-6:0.1:1))
+savefig("estimated_exposure.png")
+savefig("estimated_exposure.pdf")
 
 # Create an optimizer for the SINDY problem
-opt = STLSQ(thresholds)
+opt = SR3()
+# Create the thresholds which should be used in the search process
+thresholds = exp10.(-6:0.1:1)
 
-
-# тут код с X и L: определяем проблему с правой частью
-# на тот самый подгоняемый член и делаем СИНДи
-# возможно придётся добавить функции для выполнения СИНДи, а то че-то
-# местные не робят....
-
-
-## смотри в hudson_bay.jl за новым использованием СИНДи
-
-problem_direct = ContinuousDataDrivenProblem(X[2:4, :],DX[2:4, :])
-Ψ_direct = solve(problem_direct, basis, opt, maxiter = 50000, progress = true, denoise = true, normalize = true)
+# Test on original data and without further knowledge
+Ψ_direct = SInDy(X[2:4, :], DX[2:4, :], basis, thresholds, opt = opt, maxiter = 50000) # Fail
 println(Ψ_direct.basis)
-println(Ψ_direct.parameters)
-println(result(Ψ_direct))
-
-prob = ODEProblem(Ψ_direct, u0[2:4], tspan2, Ψ_direct.parameters)
-sol_Ψ_direct = solve(prob)
-plot!(sol_Ψ_direct)
-
-
-
-
-problem_ideal = ContinuousDataDrivenProblem(X_ext[2:4,:],DX_ext[2:4,:])
-Ψ_ideal = solve(problem_ideal, basis, opt, maxiter = 50000, progress = true, denoise = true, normalize = true)
+# Test on ideal derivative data ( not available )
+Ψ_ideal = SInDy(X[2:4, 5:end], L[5:end], basis, thresholds, opt = opt, maxiter = 50000) # Succeed
 println(Ψ_ideal.basis)
-println(Ψ_ideal.parameters)
-println(result(Ψ_ideal))
-
-prob = ODEProblem(Ψ_ideal, u0[2:4], tspan2, Ψ_ideal.parameters)
-sol_Ψ_ideal = solve(prob)
-plot!(sol_Ψ_ideal)
-
-
-
-problem_ideal_L = ContinuousDataDrivenProblem(X_ext[2:4,:], L_ext[:])
-Ψ_ideal_L = solve(problem_ideal_L, basis, opt, maxiter = 50000, progress = true, denoise = true, normalize = true)
-println(result(Ψ_ideal_L))
-
-
-
-
-
-problem = ContinuousDataDrivenProblem(X̂[:, :],L̂[:])
-Ψ = solve(problem, basis, opt, maxiter = 50000, progress = true, normalize=true, denoise=true, eval_expression=true)
+# Test on uode derivative data
+Ψ = SInDy(X̂[:, 2:end], L̂[2:end], basis, thresholds,  opt = opt, maxiter = 10000, normalize = true, denoise = true) # Succeed
 println(Ψ.basis)
-println(Ψ.parameters)
-println(result(Ψ))
 
-Ψ(u0[2:4],Ψ.parameters)
-Ψ(u0[2:4])
-
-
-## Build a ODE for the estimated system
+# Build a ODE for the estimated system
 function approx(u,p,t)
-    S,E,i,R,N,D,C = u
-    F,β0,α,κ,μ,σ,γ,d,λ = p
-    z = Ψ([S/N,i,D/N])
-    dS = -β0*S*F/N - z[1] - μ*S
-    dE = β0*S*F/N + z[1] - (σ+μ)*E
-    dI = σ*E - (γ+μ)*i
-    dR = γ*i - μ*R
-    dN = -μ*N
-    dD = d*γ*i - λ*D
-    dC = σ*E
+    S,E,I,R,N,D,C = u
+    F, β0,α,κ,μ,σ,γ,d,λ = p_
+    z = Ψ([S/N,I,D/N]) # Exposure does not depend on exposed, removed, or cumulative!
+    dS = -β0*S*F/N - z[1] -μ*S # susceptible
+    dE = β0*S*F/N + z[1] -(σ+μ)*E # exposed
+    dI = σ*E - (γ+μ)*I # infected
+    dR = γ*I - μ*R # removed (recovered + dead)
+    dN = -μ*N # total population
+    dD = d*γ*I - λ*D # severe, critical cases, and deaths
+    dC = σ*E # +cumulative cases
 
     [dS,dE,dI,dR,dN,dD,dC]
 end
 
-
+# Create the approximated problem and solution
 a_prob = ODEProblem{false}(approx, u0, tspan2, p_)
 a_solution = solve(a_prob, Tsit5())
 
